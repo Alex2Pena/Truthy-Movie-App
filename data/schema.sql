c608b0db1fc5980c02449f367d42fb7c16cd314f
Alex2Pena <attn.Alex.P@gmail.com>
2020-04-02 09:50:15 -0700

    updated schema

--- data/schema.sql
+++ data/schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE items (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     picture TEXT,
-    locations text,
-    providerIcon text
+    locations TEXT,
+    providericon TEXT
 );
 
 SELECT * FROM items;
--- server.js
+++ server.js
@@ -33,41 +33,41 @@ app.post('/favorites', handleFavorites);
 app.post('/deleteFavorites', deleteFavorites);
 
 function deleteFavorites (request, response) {
-  console.log("id", request.body.id);
-  
+  console.log('id', request.body.id);
+
   let id = request.body.id;
   let sql = 'DELETE FROM items WHERE id=$1;';
   let safeValues = [id];
 
   client.query(sql, safeValues)
-      .then(() => {
-          response.redirect('/favorites');
-      })
+    .then(() => {
+      response.redirect('/favorites');
+    })
 }
 
 function renderFavorites (request, response){
-    let sql = 'Select * FROM items;';
-    client.query(sql)
+  let sql = 'Select * FROM items;';
+  client.query(sql)
     .then(res => {
       let videos = res.rows;
       // console.log('videos',videos);
-        response.render('./favorites', ({apples : videos}));
-})};
+      response.render('./favorites', ({apples : videos}));
+    })}
 
 function handleFavorites (request, response){
 // console.log('favorites request', request.body);
 
-    let{name, picture, locations, providerIcon} = request.body;
-    let sql = 'INSERT INTO items (name, picture, locations, providerIcon) VALUES ($1, $2, $3, $4);';
-    let safeValues = [name, picture, locations, providerIcon];
-    
-    client.query(sql, safeValues)
+  let{name, picture, locations, providericon} = request.body;
+  let sql = 'INSERT INTO items (name, picture, locations, providericon) VALUES ($1, $2, $3, $4);';
+  let safeValues = [name, picture, locations, providericon];
+
+  client.query(sql, safeValues)
     // .then(response => {
     //     // let id = results.rows.id;
     //     response.render('./favorites');
     .then(() => {
       response.redirect('/favorites');
-  })
+    })
     // })
 }
 
@@ -78,7 +78,7 @@ var videoArray = [];
 
 function handleSearch (req, res){
   videoArray = [];
-   let xyz = req.query.search
+  let xyz = req.query.search
   const options = {
     method: 'GET',
     //   type: 'JSON',
@@ -87,31 +87,31 @@ function handleSearch (req, res){
     headers: {
       'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
       'x-rapidapi-key': '38e058ddb8msh0ab4bb9902ac5b2p1d7aa3jsn10ae3807ccee'}};
-        request(options, function (error, response, body) {
-          if (error) throw new Error(error);
-          let allResults = JSON.parse(response.body);
-          allResults.results.forEach(result=>{
-            new Video(result);});
-          res.render('./index', {bananas: videoArray})}
-)};
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    let allResults = JSON.parse(response.body);
+    allResults.results.forEach(result=>{
+      new Video(result);});
+    res.render('./index', {bananas: videoArray})}
+  )}
 
 function Video(obj){
   this.name = obj.name;
   this.picture = obj.picture;
   this.locations = obj.locations.map((value) => {
-      return value.display_name;
+    return value.display_name;
   })
-  this.providerIcon = obj.locations.map((value) => {
+  this.providericon = obj.locations.map((value) => {
     // console.log("inside .map", value.icon);
     return value.icon;
-})
-// console.log('this.providerIcon', this.providerIcon);
-videoArray.push(this);
-};
+  })
+  // console.log('this.providericon', this.providericon);
+  videoArray.push(this);
+}
 
 client.connect()
-    .then(() => {
-  app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
-  })
-});
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`listening on ${PORT}`)
+    })
+  });
